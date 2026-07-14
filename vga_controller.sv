42749b2a5b41b6a5795b29be347cd7dc6d03ae49
Toderita Teodor <toderitateodor@gmail.com>
2026-07-14 10:01:15 +0300

    refactor: put sync logic and image display logic in two modules

--- vga_controller.sv
+++ vga_controller.sv
@@ -9,34 +9,20 @@ module vga_controller #(
     parameter V_SYNC    = 2  ,
     parameter V_B_PORCH = 33 
 )(    
-    input              clk_i  ,
-    input              rst_ni ,
-    input logic [1:0]  sw     ,
+    input              clk_i,
+    input              rst_ni,
 
     output logic       hsync_o,
     output logic       vsync_o,
 
-    output logic [3:0] red_o  ,
-    output logic [3:0] green_o,
-    output logic [3:0] blue_o 
+    output logic [9:0] h_count_o,
+    output logic [9:0] v_count_o,
+
+    output logic       video_active_o
 );
-
-// Signals for bounce logic
-logic [9:0] obj_x;              // object x coordinates
-logic [9:0] obj_y;              // object y coordinates
-
-logic       dir_x;              // moving direction on x (0 - right, 1 - left)
-logic       dir_y;              // moving direction on y (0 - down, 1 - up)         
-
-// Object half size
-localparam SQUARE_HALF_SIZE = 50;
 
 localparam H_TOTAL    = H_ACTIVE + H_F_PORCH + H_SYNC + H_B_PORCH;
 localparam V_TOTAL    = V_ACTIVE + V_F_PORCH + V_SYNC + V_B_PORCH;
-
-// Internal counters
-reg [$clog2(H_TOTAL)-1:0] h_counter;
-reg [$clog2(V_TOTAL)-1:0] v_counter;
 
 localparam H_SYNC_START = H_ACTIVE + H_F_PORCH;
 localparam H_SYNC_END   = H_ACTIVE + H_F_PORCH + H_SYNC;
@@ -44,91 +30,33 @@ localparam H_SYNC_END   = H_ACTIVE + H_F_PORCH + H_SYNC;
 localparam V_SYNC_START = V_ACTIVE + V_F_PORCH;
 localparam V_SYNC_END   = V_ACTIVE + V_F_PORCH + V_SYNC;
 
-// Change animation speed
-logic [1:0] speed_step = (sw[0]) ? 3 : 1;
+// End of line indicator
+logic h_last = (h_count_o == H_TOTAL - 1);
+logic v_last = (v_count_o == V_TOTAL - 1);
 
 // Generates the horizontal synchronization pulse required to signal the end of a line scan.
-assign hsync_o = ~((h_counter >= H_SYNC_START) &
-                   (h_counter <  H_SYNC_END));
+assign hsync_o = ~((h_count_o >= H_SYNC_START) &
+                   (h_count_o <  H_SYNC_END));
 
 // Generates the vertical synchronization pulse required to signal the end of a frame scan.
-assign vsync_o = ~((v_counter >= V_SYNC_START) &
-                   (v_counter <  V_SYNC_END));
-
-// Bounding box condition to determine if the current beam coordinates fall within the square area.
-logic is_square = (h_counter >= obj_x - SQUARE_HALF_SIZE & h_counter < obj_x + SQUARE_HALF_SIZE & 
-                  v_counter >= obj_y - SQUARE_HALF_SIZE & v_counter < obj_y + SQUARE_HALF_SIZE);
-
-// End of line indicator
-logic h_last = (h_counter == H_TOTAL - 1);
-logic v_last = (v_counter == V_TOTAL - 1);
-
-// Visible screen area detection (disabled during reset)
-logic video_active = (h_counter < H_ACTIVE) & (v_counter < V_ACTIVE) & rst_ni;
-
-// Color output logic
-always_comb begin
-    red_o   = 4'h0;
-    green_o = 4'h0;
-    blue_o  = 4'h0;
-
-    if (video_active) begin
-        if (is_square) begin
-            if (sw[1]) begin
-                red_o   = 4'h0;
-                green_o = 4'hF;
-                blue_o  = 4'hF;
-            end else begin
-                red_o   = 4'hF;
-                green_o = 4'hF;
-                blue_o  = 4'h0;
-            end
-        end else begin
-            red_o   = 4'hF;
-            green_o = 4'h0;
-            blue_o  = 4'h0;
-        end
-    end
-end
-
-// Object X position logic
-always_ff @(posedge clk_i or negedge rst_ni) begin
-if(!rst_ni)          obj_x <= H_ACTIVE / 2; else
-if(h_last && v_last) obj_x <= (!dir_x) ? obj_x + speed_step : obj_x - speed_step;
-end
-
-// Object Y position logic
-always_ff @(posedge clk_i or negedge rst_ni) begin
-if(!rst_ni)          obj_y <= V_ACTIVE / 2; else
-if(h_last && v_last) obj_y <= (!dir_y) ? obj_y + speed_step : obj_y - speed_step;
-end
-
-// X direction bounce logic
-always_ff @(posedge clk_i or negedge rst_ni) begin
-if(!rst_ni)                                      dir_x <= 0; else
-if(obj_x + SQUARE_HALF_SIZE + speed_step >= H_ACTIVE) dir_x <= 1; else
-if(obj_x <= SQUARE_HALF_SIZE + speed_step)            dir_x <= 0;
-end
-
-// Y direction bounce logic 
-always_ff @(posedge clk_i or negedge rst_ni) begin
-if(!rst_ni)                                      dir_y <= 0; else
-if(obj_y + SQUARE_HALF_SIZE + speed_step >= V_ACTIVE) dir_y <= 1; else
-if(obj_y <= SQUARE_HALF_SIZE + speed_step)            dir_y <= 0;
-end
+assign vsync_o = ~((v_count_o >= V_SYNC_START) &
+                   (v_count_o <  V_SYNC_END));
 
 // Horizontal counter logic
 always_ff @(posedge clk_i or negedge rst_ni) begin
-if(!rst_ni) h_counter <= 0; else
-if(h_last)  h_counter <= 0; else
-            h_counter <= h_counter + 1;
+if(!rst_ni) h_count_o <= 0; else
+if(h_last)  h_count_o <= 0; else
+            h_count_o <= h_count_o + 1;
 end
 
 // Vertical counter logic
 always_ff @(posedge clk_i or negedge rst_ni) begin
-if(!rst_ni)          v_counter <= 0; else 
-if(h_last && v_last) v_counter <= 0; else
-if(h_last)           v_counter <= v_counter + 1;
+if(!rst_ni)          v_count_o  <= 0; else 
+if(h_last && v_last) v_count_o <= 0; else
+if(h_last)           v_count_o <= v_count_o + 1;
 end
+
+// Visible screen area detection (disabled during reset)
+assign video_active_o = (h_count_o < H_ACTIVE) & (v_count_o < V_ACTIVE) & rst_ni;
 
 endmodule
---
+++ vga_image_display.sv
@@ -0,0 +1,100 @@
+module vga_image_display #(
+    parameter H_ACTIVE  = 640,
+    parameter H_F_PORCH = 16 ,
+    parameter H_SYNC    = 96 ,
+    parameter H_B_PORCH = 48 ,
+
+    parameter V_ACTIVE  = 480,
+    parameter V_F_PORCH = 10 ,
+    parameter V_SYNC    = 2  ,
+    parameter V_B_PORCH = 33 
+)(    
+    input              clk_i  ,
+    input              rst_ni ,
+
+    input logic [1:0]  sw     ,
+
+    input logic [9:0]  h_counter,
+    input logic [9:0]  v_counter,
+    input logic        video_active,
+
+    output logic [3:0] red_o  ,
+    output logic [3:0] green_o,
+    output logic [3:0] blue_o 
+);
+
+localparam H_TOTAL = H_ACTIVE + H_F_PORCH + H_SYNC + H_B_PORCH;
+localparam V_TOTAL = V_ACTIVE + V_F_PORCH + V_SYNC + V_B_PORCH;
+
+// End of line indicator calculat intern
+logic h_last = (h_counter == H_TOTAL - 1);
+logic v_last = (v_counter == V_TOTAL - 1);
+
+// Signals for bounce logic
+logic [9:0] obj_x;              // object x coordinates
+logic [9:0] obj_y;              // object y coordinates
+
+logic       dir_x;              // moving direction on x (0 - right, 1 - left)
+logic       dir_y;              // moving direction on y (0 - down, 1 - up)         
+
+// Object half size
+localparam SQUARE_HALF_SIZE = 50;
+
+// Change animation speed
+logic [1:0] speed_step = (sw[0]) ? 3 : 1;
+
+// Bounding box condition to determine if the current beam coordinates fall within the square area.
+logic is_square = ((h_counter >= (obj_x - SQUARE_HALF_SIZE)) & (h_counter < (obj_x + SQUARE_HALF_SIZE)) & 
+                  (v_counter >= (obj_y - SQUARE_HALF_SIZE)) & (v_counter < (obj_y + SQUARE_HALF_SIZE)));
+
+// Color output logic
+always_comb begin
+    red_o   = 4'h0;
+    green_o = 4'h0;
+    blue_o  = 4'h0;
+    if (video_active) begin
+        if (is_square) begin
+            if (sw[1]) begin
+                red_o   = 4'h0;
+                green_o = 4'hF;
+                blue_o  = 4'hF;
+            end else begin
+                red_o   = 4'hF;
+                green_o = 4'hF;
+                blue_o  = 4'h0;
+            end
+        end else begin
+            red_o   = 4'hF;
+            green_o = 4'h0;
+            blue_o  = 4'h0;
+        end
+    end
+end
+
+// Object X position logic
+always_ff @(posedge clk_i or negedge rst_ni) begin
+if(!rst_ni)          obj_x <= H_ACTIVE / 2; else
+if(h_last && v_last) obj_x <= (!dir_x) ? (obj_x + speed_step) : (obj_x - speed_step);
+end
+
+// Object Y position logic
+always_ff @(posedge clk_i or negedge rst_ni) begin
+if(!rst_ni)          obj_y <= V_ACTIVE / 2; else
+if(h_last && v_last) obj_y <= (!dir_y) ? (obj_y + speed_step) : (obj_y - speed_step);
+end
+
+// X direction bounce logic
+always_ff @(posedge clk_i or negedge rst_ni) begin
+if(!rst_ni)                                             dir_x <= 0; else
+if((obj_x + SQUARE_HALF_SIZE + speed_step) >= H_ACTIVE) dir_x <= 1; else
+if(obj_x <= (SQUARE_HALF_SIZE + speed_step))            dir_x <= 0;
+end
+
+// Y direction bounce logic 
+always_ff @(posedge clk_i or negedge rst_ni) begin
+if(!rst_ni)                                             dir_y <= 0; else
+if((obj_y + SQUARE_HALF_SIZE + speed_step) >= V_ACTIVE) dir_y <= 1; else
+if(obj_y <= (SQUARE_HALF_SIZE + speed_step))            dir_y <= 0;
+end
+
+endmodule
