36da2a614e619d9498fb0b73976def2261bf65b7
Toderita Teodor <toderitateodor@gmail.com>
2026-07-14 10:01:48 +0300

    refactor: add new instance

--- vga_controller_top.sv
+++ vga_controller_top.sv
@@ -27,6 +27,9 @@ logic vga_clk;
 // reset active low
 logic rst_n = ~reset;
 
+logic [9:0] w_h_counter;
+logic [9:0] w_v_counter;
+logic       w_video_active;
 
 vga_controller #(
     .H_ACTIVE (H_ACTIVE ),
@@ -38,17 +41,34 @@ vga_controller #(
     .V_SYNC   (V_SYNC   ),
     .V_B_PORCH(V_B_PORCH)
 ) vga_controller_inst (    
-    .clk_i  (vga_clk),
-    .rst_ni (rst_n),
+    .clk_i          (vga_clk),
+    .rst_ni         (rst_n),
+    .hsync_o        (Hsync),
+    .vsync_o        (Vsync),
+    .h_count_o      (w_h_counter),
+    .v_count_o      (w_v_counter),
+    .video_active_o (w_video_active)
+);
 
-    .sw     (sw),
-
-    .hsync_o(Hsync),
-    .vsync_o(Vsync),
-
-    .red_o  (vgaRed  ),
-    .green_o(vgaGreen),
-    .blue_o (vgaBlue )
+vga_image_display #(
+    .H_ACTIVE (H_ACTIVE ),
+    .H_F_PORCH(H_F_PORCH),
+    .H_SYNC   (H_SYNC   ),
+    .H_B_PORCH(H_B_PORCH),
+    .V_ACTIVE (V_ACTIVE ),
+    .V_F_PORCH(V_F_PORCH),
+    .V_SYNC   (V_SYNC   ),
+    .V_B_PORCH(V_B_PORCH)
+) vga_image_display_inst (
+    .clk_i        (vga_clk),
+    .rst_ni       (rst_n),
+    .sw           (sw),
+    .h_counter    (w_h_counter),
+    .v_counter    (w_v_counter),
+    .video_active (w_video_active),
+    .red_o        (vgaRed),
+    .green_o      (vgaGreen),
+    .blue_o       (vgaBlue)
 );
 
 clk_vga_wrapper clk_vga_wrapper_inst(
