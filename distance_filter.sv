d1dada312d6f2b9fbf5f6e7710fa66a847eeb0c8
Toderita Teodor <toderitateodor@gmail.com>
2026-07-23 19:27:10 +0300

    feat: add moving average filter

--- distance_filter.sv
+++ distance_filter.sv
@@ -1,41 +1,39 @@
 module distance_filter (
-    input  logic        clk_i,
-    input  logic        rst_ni,
-    input  logic        sample_valid_i, 
-    input  logic [9:0]  raw_dist_i,     
+    input  logic        clk_i          ,
+    input  logic        rst_ni         ,
+    input  logic        sample_valid_i ,
+    input  logic [9:0]  raw_dist_i     ,
 
-    output logic [9:0]  filtered_dist_o 
+    output logic [9:0]  filtered_dist_o
 );
 
-logic [9:0] sample0, sample1, sample2, sample3;
+// Four-sample moving average
 logic [11:0] sum;
+logic [9:0] sample0, sample1, sample2;
 
+// Sample history
 always_ff @(posedge clk_i or negedge rst_ni) begin
 if(!rst_ni)         sample0 <= 0; else
-if(sample_valid_i) sample0 <= raw_dist_i;
-end 
+if(sample_valid_i)  sample0 <= raw_dist_i;
+end
 
 always_ff @(posedge clk_i or negedge rst_ni) begin
 if(!rst_ni)         sample1 <= 0; else
-if(sample_valid_i) sample1 <= sample0;
-end 
+if(sample_valid_i)  sample1 <= sample0;
+end
 
 always_ff @(posedge clk_i or negedge rst_ni) begin
 if(!rst_ni)         sample2 <= 0; else
-if(sample_valid_i) sample2 <= sample1;
-end 
+if(sample_valid_i)  sample2 <= sample1;
+end
 
-always_ff @(posedge clk_i or negedge rst_ni) begin
-if(!rst_ni)         sample3 <= 0; else
-if(sample_valid_i) sample3 <= sample2;
-end 
-
-
+// Sum the latest four samples
 always_ff @(posedge clk_i or negedge rst_ni) begin
 if(!rst_ni)         sum <= 0; else
-if(sample_valid_i) sum <= raw_dist_i + sample0 + sample1 + sample2;;
-end 
+if(sample_valid_i)  sum <= raw_dist_i + sample0 + sample1 + sample2;
+end
 
+// Divide by four
 assign filtered_dist_o = sum >> 2;
 
 endmodule
